cd09cf0fd5532d11d8136030beccbfc9569a9ca7
javaherobrine <49926366+javaherobrine@users.noreply.github.com>
2022-08-12 00:07:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#OI
+I'm just an OIer,i wanna share some problems and ways to solve them with OIers.<br />
+Ways to solve problems is shown in annocations.
---
+++ template.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize(3)
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	ifstream fin{"输入文件"};
+	ofstream fout{"输出文件"};
+}
