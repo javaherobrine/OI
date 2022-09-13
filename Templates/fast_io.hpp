922af04007bc1e0d77b2c7c1f1dd38f4e841f7f9
javaherobrine <49926366+javaherobrine@users.noreply.github.com>
2022-09-13 13:19:58 +0800

    use bit operation to make quick read faster

--- Templates/fast_io.hpp
+++ Templates/fast_io.hpp
@@ -2,6 +2,8 @@
  * Quick read and quick write based on iostream
  * Faster than getchar() and putchar()
  * Remember to use ios::sync_with_stdio(false) and cin.tie(nullptr)
+ * Just use IO::inbuf=cin.rdbuf() and IO::outbuf=cout.rdbuf()
+ * Then use read and write
  */
 #include<iostream>
 using namespace std;
@@ -19,27 +21,26 @@ long long read(){
 	char ch=IO::getchar();
 	long long sgn{1},val{};
 	while(ch<'0'||ch>'9'){
-		if(ch=='-'){
+		if(ch=='-'){//get the number's sign
 			sgn*=-1;
 		}
 		ch=IO::getchar();
 	}
 	while(ch>='0'&&ch<='9'){
-		val*=10;
-		val+=ch-'0';
+		val=val<<3+val<<1;//area for the next digit
+		val+=ch^48;//input the next digit
 		ch=IO::getchar();
 	}
-	return sgn*val;
+	return sgn*val;//the value of the number is its sign multiplies its absolute value
 }
 void write(long long val){
-	if(val<0){
+	if(val<0){//if it's a negative number
 		IO::putchar('-');
 		val=-val;
 	}
-	if(val==0){
-		return;
-	}else{
+	if(val>9){//print its high bit
+		//Why can't I use if(val==0) return;?This is your homework!
 		write(val/10);
-		IO::putchar(val%10+'0');
 	}
+	IO::putchar(val%10+'0');
 }
