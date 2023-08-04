f533b889966cd7eed0d5a1b7007a77d2d4342235
javaherobrine <49926366+javaherobrine@users.noreply.github.com>
2023-08-04 18:20:45 +0800

    fix a bug of debug.hpp

--- debug.hpp
+++ debug.hpp
@@ -1,15 +1,40 @@
+/*
+ * A header file for me to debug
+ * use output function to output a single variable
+ * use multi_output function to output a number of variables
+ * use #define NSTDERR to disable stderr
+ * if there is no macro named NSTDERR, your stdout will be replaced with stderr
+ * otherwise, your stderr will be replaced with stdout
+ * or the output will get mixed
+ */
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
 #include<bits/stdc++.h>
 using namespace std;
-#ifndef ECLIPSE
+#ifndef NSTDERR
 #define cout cerr
+#else
+#define cerr cout
 #endif
+template<typename T>
+void output(const T& val);
+template<typename T>
+void output(const vector<T>& val);
+template<typename T>
+void output(const deque<T>& val);
+template<typename K,typename V>
+void output(const pair<K,V>& val);
+template<typename K,typename V>
+void output(const unordered_map<K,V>& val);
+template<typename K,typename V>
+void output(const map<K,V>& val);
 template<typename T>
 void output(const T& val){
 	cerr<<val;
 }
 template<typename T>
 void output(const vector<T>& val){
-	for(T& v:val){
+	for(auto&& v:val){
 		output(v);
 		cerr<<" ";
 	}
@@ -17,7 +42,7 @@ void output(const vector<T>& val){
 }
 template<typename T>
 void output(const deque<T>& val){
-	for(T& v:val){
+	for(auto&& v:val){
 		output(v);
 		cerr<<" ";
 	}
@@ -33,7 +58,7 @@ void output(const pair<K,V>& val){
 }
 template<typename K,typename V>
 void output(const unordered_map<K,V>& val){
-	for(pair<K,V>& v:val){
+	for(auto&& v:val){
 		output(v);
 		cerr<<" ";
 	}
@@ -41,7 +66,7 @@ void output(const unordered_map<K,V>& val){
 }
 template<typename K,typename V>
 void output(const map<K,V>& val){
-	for(pair<K,V>& v:val){
+	for(auto&& v:val){
 		output(v);
 		cerr<<" ";
 	}
@@ -58,3 +83,4 @@ void multi_output(const T& value,const Args&... args){
 		cerr<<endl;
 	}
 }
+#endif
